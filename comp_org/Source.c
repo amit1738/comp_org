d88c31c93c477ca23820b5cfa589f8904a410a93
Amit Sharvit <amitsharvit@mail.tau.ac.il>
2025-01-09 17:30:42 +0200

    imemin file for test

--- comp_org/Source.c
+++ comp_org/Source.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 
+int initialize();
+char fetch(int lineNumber);
+
 int main()
 {
-	initalize();
+	initialize();
+	fetch(1);
     return 0;
 }
 
 
+char fetch(int lineNumber)
+{
+    //fetch the instruction from the memory
+    FILE* fetchP = fopen("imemin.txt", "r");
+    char line[256];
+    if (fetchP != NULL)
+    {
+        int line_length = 32; // Fixed length of each line (including newline)
+
+        // Calculate byte offset and seek
+        long offset = (lineNumber - 1) * line_length; // Offset for the target line
+        fseek(fetchP, offset, SEEK_SET);
+
+        // Read the target line
+        if (fgets(line, sizeof(line), fetchP) != NULL)
+        {
+            printf("Line %d: %s", lineNumber, line);
+        }
+        else {
+            printf("Error: Could not fetch line %d.\n", lineNumber);
+    }
+	}
+	else {
+		printf("Error: Could not open file.\n");
+	}
+    fclose(fetchP);
+    	return 0;
+
+}
+
 //lets initialize the project
-int initalize()
+int initialize()
 {
 	///pointers to the files
     FILE* dmemout = fopen("dmemout.txt", "w");
@@ -19,6 +53,7 @@ int initalize()
     FILE* leds = fopen("leds.txt", "w");
     FILE* display7seg = fopen("display7seg.txt", "w");
 
+    //inilize zeros in regs
     int i= 0;
     unsigned char zero = 0x0;
     for (i = 0; i < 12; i++)

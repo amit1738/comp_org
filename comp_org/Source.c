f5d5e4f8ef6641bd3c822642dfe71c4f80caaf25
Amit Sharvit <amitsharvit@mail.tau.ac.il>
2025-01-09 16:02:51 +0200

    initalize simut

--- comp_org/Source.c
+++ comp_org/Source.c
@@ -2,19 +2,7 @@
 
 int main()
 {
-    char* filename = "test.txt";
-
-    // open the file for writing
-    FILE* fp = fopen(filename, "w");
-    if (fp == NULL)
-    {
-        printf("Error opening the file %s", filename);
-        return -1;
-    }d
-
-    // close the file
-    fclose(fp);
-
+	initalize();
     return 0;
 }
 
@@ -22,6 +10,29 @@ int main()
 //lets initialize the project
 int initalize()
 {
-    char* 
+	///pointers to the files
+    FILE* dmemout = fopen("dmemout.txt", "w");
+    FILE* regout = fopen("regout.txt", "w");
+    FILE* trace = fopen("trace.txt", "w");
+    FILE* hwregtrace = fopen("hwregtrace.txt", "w");
+    FILE* cycles = fopen("cycles.txt", "w");
+    FILE* leds = fopen("leds.txt", "w");
+    FILE* display7seg = fopen("display7seg.txt", "w");
+
+    int i= 0;
+    unsigned char zero = 0x0;
+    for (i = 0; i < 12; i++)
+    {
+        fprintf(regout,"%x\n",zero);
+    }
+    //clsoe the files
+    fclose(dmemout);
+    fclose(regout);
+    fclose(hwregtrace);
+    fclose(trace);
+    fclose(cycles);
+    fclose(leds);
+    fclose(display7seg);
+
 	return 0;
 }
